142cbf8226d92825942501d4c5b44c9d127477bd
VivianGomez <30846540+VivianGomez@users.noreply.github.com>
2018-08-27 07:53:31 -0500

    Update global.js

--- js/global.js
+++ js/global.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('a[href^="#"]').click(function() {
     var destino = $(this.hash);
-    $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
+    $('html, body').animate({ scrollTop: destino.offset().top }, 2000);
     return false;
   });
 });
